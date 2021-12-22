e143af33638725fe87b6fb25752ae046f20c66bc
KUESTER <kuester@de.ibm.com>
2021-12-22 23:08:53 +0100

    Fix React film list

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -11,5 +11,4 @@ const Movie = (props) => {
     </li>
   );
 };
-
 export default Movie;
--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -8,7 +8,7 @@ const MovieList = (props) => {
     <ul className={classes['movies-list']}>
       {props.movies.map((movie) => (
         <Movie
-          key={Math.random() *10 }
+          key={ Math.random() *10 }
           id={movie.id}
           title={movie.title}
           releaseDate={movie.releaseDate}
